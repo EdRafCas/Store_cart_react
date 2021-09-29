493bc2a73601ab7c28654f6929f4e403318e9754
EdRafCas <Eduardoo.rafael@gmail.com>
2021-09-28 22:34:59 -0300

    small changes to collection css

--- src/Components/Colection.js
+++ src/Components/Colection.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import ProductGallery from './ProductGallery';
+import {Link} from "react-router-dom";
 
 const GalleryContainer = styled.div`
       width: 100%;
@@ -15,21 +16,45 @@ const ColectionContainer =styled.div`
       width: 100%;
       display: flex;
       flex-direction:column;
-      `
+`
 const ColectionBar =styled.div`
       width:100%;
-      display:flex;
+      display: grid;
+      gap:00px;
+      grid-template-columns: 3fr 1fr;
+`
+
+const OrderByContainer =styled.div`
+      width: 100%;
+      display: flex;
       flex-direction:row;
-      justify-content:space-between;
-      `
+      justify-content:flex-end;
+      
+`
+const RedirectButtom =styled(Link)`
+      font-size:16px;
+      display:inline-block;
+      width:auto;
+      padding:15px 15px 15px 0px;
+      text-decoration:none;
+      color:#000;
+
+      &:hover{
+            text-decoration:underline;
+            color:#000;
+      }
+`
 
 const Colection = () => {
       return ( 
             <ColectionContainer>
                   
                   <ColectionBar>
-                        <h1>2</h1>
-                        <h1>2</h1>
+                        <RedirectButtom to="/">Main</RedirectButtom>
+                        <OrderByContainer>
+                              <span>1</span>
+                              <span>2</span>
+                        </OrderByContainer>
                   </ColectionBar>
                   <GalleryContainer>
                         <FilterBar/>
